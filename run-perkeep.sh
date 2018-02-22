bfd74b89c7b0ae7236dd8516ba449d871d09d314
James Hillyerd <james@hillyerd.com>
2018-02-21 18:29:01 -0800

    Less vars in run script

--- run-perkeep.sh
+++ run-perkeep.sh
@@ -5,14 +5,12 @@
 CONFIG="/config/server-config.json"
 KEYRING="/config/identity-secring.gpg"
 USERPASS="perkeep:perkeep"
-GENKEY="/usr/bin/genkey"
-PERKEEP="/usr/bin/camlistored"
 
 set -e
 
 if test ! -f "$CONFIG" -a ! -f "$KEYRING"; then
   echo "$0: Creating $KEYRING"
-  KEY="$($GENKEY -secring "$KEYRING")"
+  KEY="$(/usr/bin/genkey -secring "$KEYRING")"
   echo "$0: New key ID: $KEY"
   echo "$0: Creating $CONFIG"
   echo "$0: Initial user:passwd is $USERPASS - please change it!"
@@ -31,4 +29,4 @@ cat >"$CONFIG" << EOT
 EOT
 fi
 
-exec $PERKEEP -configfile "$CONFIG"
+exec /usr/bin/camlistored -configfile "$CONFIG"
